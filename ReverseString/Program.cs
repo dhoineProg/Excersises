dfce2a29dafe0e1e1a0253fd77f508ffa20488a7
Эмиль Буреев <emilbureev790@gmail.com>
2023-08-17 11:49:23 +0300

    Добавлено условие для проверки входящей строки на наличие в строке только английских симолов в нижнем регистре

--- ReverseString/Program.cs
+++ ReverseString/Program.cs
@@ -3,25 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Part1
 {
     internal class Program
     {
+        static bool ExaminationString(string inputS)
+        {
+            try
+            {
+                foreach(char letter in inputS)
+                {
+                    if (!Char.IsLower(letter) || !Char.IsLetter(letter) || !Regex.IsMatch(inputS, @"\P{IsCyrillic}"))
+                    {
+                        return false;
+                    } 
+                }
+                return true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return true;
+        }
+
         static string ProcessString(string inputS)
         {
-            if (inputS.Length % 2 == 0)
-            {
-                int hLength = inputS.Length / 2;
-                string subString1 = ReverseString(inputS.Substring(0, hLength));
-                string subString2 = ReverseString(inputS.Substring(hLength));
-                return subString1 + subString2;
-            }
-            else
-            {
-                string reversedString = ReverseString(inputS);
-                return reversedString + inputS;
-            }
+                if (inputS.Length % 2 == 0)
+                {
+                    int hLength = inputS.Length / 2;
+                    string subString1 = ReverseString(inputS.Substring(0, hLength));
+                    string subString2 = ReverseString(inputS.Substring(hLength));
+                    return subString1 + subString2;
+                }
+                else
+                {
+                    string reversedString = ReverseString(inputS);
+                    return reversedString + inputS;
+                }
 
         }
 
@@ -36,10 +57,16 @@ namespace Part1
         {
             Console.WriteLine("Введите строку: ");
             string inputS = Console.ReadLine();
+            if (ExaminationString(inputS))
+            {
+                string procesString = ProcessString(inputS);
 
-            string procesString = ProcessString(inputS);
-
-            Console.WriteLine(procesString);
+                Console.WriteLine(procesString);
+            }
+            else
+            {
+                Console.WriteLine("Проверьте правильность написания строки.");
+            }
 
             Console.ReadLine();
         }
